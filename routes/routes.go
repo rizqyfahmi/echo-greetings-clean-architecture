dbe33223d497738997f5bba49d18e05562a24154
rizqyfahmi <rizqyfahmi@yahoo.com>
2023-09-17 08:03:15 +0700

    feat: load html file

--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,11 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	"github.com/rizqyfahmi/gin-greetings-clean-architecture/constant"
+	CustomErrorPackage "github.com/rizqyfahmi/gin-greetings-clean-architecture/pkg/custom_error"
+	LoggerPackage "github.com/rizqyfahmi/gin-greetings-clean-architecture/pkg/logger"
 )
 
 type Routes interface {
@@ -30,6 +35,22 @@ func (r *RoutesImpl) GetEngine() *gin.Engine {
 }
 
 func (r *RoutesImpl) Run() {
+	path := "Routes:Run"
+	defer func() {
+		if err := recover(); err != nil {
+			err = CustomErrorPackage.NewCustomError(
+				constant.ErrRoutes,
+				err.(error),
+				path,
+			)
+
+			LoggerPackage.WriteLog(logrus.Fields{
+				"path":  err.(*CustomErrorPackage.CustomError).GetPath(),
+				"title": err.(*CustomErrorPackage.CustomError).GetDisplay().Error(),
+			}).Panic(err.(*CustomErrorPackage.CustomError).GetPlain())
+		}
+	}()
+
 	r.engine.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"}, //http or https
 		AllowMethods:     []string{"GET", "POST"},
@@ -38,7 +59,9 @@ func (r *RoutesImpl) Run() {
 		MaxAge:           12 * time.Hour,
 	}))
 
+	r.engine.LoadHTMLGlob("templates/*.html")
+
 	r.engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "Hello World")
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 }
